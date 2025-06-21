d74fd1523ab22d1c2eb7398e9da875253c601315
roberto8546 <tbarobmorales@ugmnorte.edu.mx>
2025-06-20 21:04:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("datoCuriosoBtn").addEventListener("click", function () {
+  alert("¿Sabías que? Los nematodos pueden formar quistes en las raíces de las plantas y algunos sobreviven más de 10 años sin un huésped.");
+});
